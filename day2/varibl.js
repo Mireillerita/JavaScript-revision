74875dc18292c255eb0946584c6692e756714900
Mireillerita <mireillendahimana@gmail.com>
2024-02-26 05:34:44 -0800

    my javaScript revision

---
+++ day2/varibl.js
@@ -0,0 +1,14 @@
+/* types of variables*/
+var age = 18;
+var my_name = "rita";
+console.log(my_name + age);
+console.log(`my name is ${my_name} and my age is ${age}`);
+
+const array = [23, "rita", false];
+console.log(array);
+//function
+function myname(){
+    let me = "mireille";
+    console.log(me);
+}
+myname()
