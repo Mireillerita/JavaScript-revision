5fbaf627964cadd18ed015c5d790627701ed793f
Mireillerita <mireillendahimana@gmail.com>
2024-02-29 01:51:28 -0800

    Math function in javaScript

---
+++ day4/day4.js
@@ -0,0 +1,29 @@
+//javaScipt math function
+let num =-6;
+console.log(Math.abs(num));
+let number =2.7
+console.log(Math.ceil(number));
+console.log(Math.round(number));
+console.log(Math.floor(number));
+// maximum number and minimum
+console.log(Math.max(num,number));
+console.log(Math.min(num,number));
+
+//power exponantion
+console.log(Math.pow(9,2));
+
+//square root of a number
+
+console.log(Math.sqrt(144));
+ //random number
+ console.log(Math.random());
+ //trigonometric function
+ console.log(Math.sin(60));
+ console.log(Math.cos(60));
+ console.log(Math.tan(60));
+ //logarithimic function
+
+ console.log(Math.log(90));
+ console.log(Math.exp(90));
+
+
